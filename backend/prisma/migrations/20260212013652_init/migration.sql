5a87596bd97c47504e8be9ee12506ae0f63f9031
JoaoGabriel04 <jgabrielcastro04@gmail.com>
2026-03-03 10:41:21 -0300

    Docker implements

---
+++ backend/prisma/migrations/20260212013652_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `locked` on the `agendas` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "agendas" DROP COLUMN "locked";
