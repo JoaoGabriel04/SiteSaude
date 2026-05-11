19310c45ffaed9f2a2d2977513ec213553bc608b
JoaoGabriel04 <jgabrielcastro04@gmail.com>
2026-05-10 23:19:53 -0300

    fix: user type update

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -30,27 +30,3 @@ export enum Role {
   MEDICO = "MEDICO",
   ATENDENTE = "ATENDENTE"
 }
-
-export interface Atendente {
-  userId: string;
-  setor: string;
-}
-
-export interface User {
-  id: string;
-  nome: string;
-  cpf: string;
-  nascimento: string;
-  fone: string;
-  email: string;
-  avatar: string | null;
-  role: "ADMIN" | "MEDICO" | "ATENDENTE" | null;
-  medico?: Medico | null;
-  atendente?: Atendente | null;
-}
-
-export enum Role {
-  ADMIN = "ADMIN",
-  MEDICO = "MEDICO",
-  ATENDENTE = "ATENDENTE"
-}
