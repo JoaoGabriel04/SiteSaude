7bd9bffe8914b07e6ff9ab797c9e2fdeec2a61cb
JoaoGabriel04 <jgabrielcastro04@gmail.com>
2026-05-26 15:45:09 -0300

    fix: adiciona www.medflowgra.uk nas origens permitidas do CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 7000;
 const urlsAllowed = [
   "http://localhost:3000",
   "https://medflowgra.vercel.app",
+  "https://www.medflowgra.uk"
 ]
 
 app.use(helmet());
